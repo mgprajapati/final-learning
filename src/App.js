a6803306a0c8e0df382fc1e0a77cbef67acbf4ce
Mehul prajapati <mgprajapati30@gmail.com>
2023-10-15 12:01:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+// import './App.css';
+import Homepage from './home-page/home-page.js';
+
+
+function App() {
+  return (
+    <Homepage />
+  );
+}
+
+export default App;
---
+++ src/home-page/home-page.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+
+export default function Homepage() {
+    return(
+        <>
+        <p>Hello home page</p>
+        </>
+    )}
